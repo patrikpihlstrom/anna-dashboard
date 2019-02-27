fd76858564410e3ef06821feb05cd03f4bab6068
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-02-27 01:02:41 +0100

    use api client

--- dashboard/jobs/views.py
+++ dashboard/jobs/views.py
@@ -1,25 +1,21 @@
-import json
-import os
-
 from django.http import HttpResponse
 from django.template import loader
-import requests
+
+from anna_client.client import Client
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    headers = {'Authorization': 'Bearer ' + str(os.environ['ANNA_TOKEN'])}
-    response = requests.get(os.environ['ANNA_HOST'] + '/v1.0/get', params={'status': ['running', 'starting', 'pending']}, headers=headers)
-    jobs = []
-    if response.status_code == 200:
-        jobs = []
-        for job in json.loads(response.content.decode('utf-8')):
-            if all(attribute in job for attribute in ('tag', 'status', 'log', 'updated_at')):
-                jobs.append({'tag': job['tag'], 'status': job['status'], 'log': job['log'][-int(job['log'].find(' ')):], 'updated_at': job['updated_at']})
-    print(jobs)
-    return HttpResponse(template.render({'jobs': jobs}, request))
+	template = loader.get_template('index.html')
+	client = Client('http://localhost:5000')
+	client.authenticate(True)
+	jobs = []
+	for job in client.query():
+		if all(attribute in job for attribute in ('tag', 'status', 'log', 'updated_at')):
+			jobs.append({'tag': job['tag'], 'status': job['status'], 'log': job['log'][-int(job['log'].find(' ')):],
+			             'updated_at': job['updated_at']})
+	return HttpResponse(template.render({'jobs': jobs}, request))
 
 
 def done(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({}, request))
+	template = loader.get_template('index.html')
+	return HttpResponse(template.render({}, request))
